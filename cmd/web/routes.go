f270ffe5f340f0f2cee0477c36efda0a8bcc848c
Amy Kintner <kintnerae@gmail.com>
2024-09-17 15:34:20 -0700

    move testing utils around

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,11 +13,13 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /ping", ping)
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standard.Then(mux)
 }
