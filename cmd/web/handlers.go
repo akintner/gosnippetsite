4f6cd2d41aceb690049c7c2bfcfe9b6aa7bc0ae0
Amy Kintner <kintnerae@gmail.com>
2024-10-11 15:32:53 -0700

    add users models and routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -92,3 +92,19 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a form for signing up a new user...")
+}
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a form for logging in a user...")
+}
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
